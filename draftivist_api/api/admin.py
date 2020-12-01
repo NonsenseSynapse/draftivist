c419b9d6f4b90cd9b111948a39248493d633bf97
Prakash Mallela <prakash.mallela@gmail.com>
2020-11-30 21:00:57 -0500

    attempt at permissioning, limiting which campaigns a user can see in the admin. however this leaves all other routes open.

--- draftivist_api/api/admin.py
+++ draftivist_api/api/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from api.models.models import Campaign, Recipient, Issue, Statement, Draft, StatementSubmission, SessionMeta, Organization, Member
 
-admin.site.register(Campaign)
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'organization', 'description', 'created', 'start_date', 'end_date', 'is_active', 'allow_custom_statements']
+    ordering = ['id']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+
+        grp = request.user.groups.first()
+        return qs.filter(organization=grp.organization)
+
+
 admin.site.register(Recipient)
 admin.site.register(Issue)
 admin.site.register(Statement)
