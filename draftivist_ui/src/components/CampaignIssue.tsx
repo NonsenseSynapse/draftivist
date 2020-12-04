07cbe75758300b14b543887b05653c25f0e804d3
Alex Cope <alexjcope@gmail.com>
2020-12-03 18:18:28 -0800

    Use vnode.attrs directly

--- draftivist_ui/src/components/CampaignIssue.tsx
+++ draftivist_ui/src/components/CampaignIssue.tsx
@@ -9,39 +9,22 @@ type Attrs = {
 
 export default function() : BaseComponent<Attrs> {
 
-    let campaign: Campaign
-    let issue: Issue
-    let selectedIssueIndex: number
-
-    function refresh(vnode: m.Vnode<Attrs, {}>) {
-        campaign = vnode.attrs.campaign
-        issue = vnode.attrs.issue
-
-        selectedIssueIndex = campaign.selectedIssues.indexOf(issue.id)
-    }
-
-    function selectStatement(id: number) {
+    function selectStatement(issue: Issue, id: number) {
         issue.selectStatement(id)
     }
 
     return {
         ...elementAttrs,
-        oninit: (vnode) => {
-            refresh(vnode)
-        },
-        onbeforeupdate: (vnode) => {
-            // todo: is this best practice? basically because we're reassigning
-            // vnode params to closure variables, we need to call this before
-            // the update so the view fn picks up the new vars...this seems dumb tbh
-            refresh(vnode)
-            return true
-        },
-        view: () =>
+        view: (vnode) => {
+            const { campaign, issue } = vnode.attrs
+            const selectedIssueIndex = campaign.selectedIssues.indexOf(issue.id)
+
+            return (
             <div>
                 <h2>{issue.description}</h2>
                 <h3>Select a statement for this issue:</h3>
                 {issue.statements.map(statement => 
-                    <li onclick={selectStatement.bind(this, statement.id)}>
+                    <li onclick={selectStatement.bind(this, issue, statement.id)}>
                         { issue.isSelected(statement.id) ? 
                             <b>{statement.description}</b> : 
                             <span>{statement.description}</span>
@@ -55,5 +38,7 @@ export default function() : BaseComponent<Attrs> {
                 }
                 </p>
             </div>
+            )
+        }
     }
 }
