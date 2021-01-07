4cf716f88e1650df7272b5bbd62bf608a9146347
Monica Barboza <barboza.monica2@gmail.com>
2021-01-07 02:25:29 -0500

    some css

--- draftivist_ui/config/webpack.config.base.js
+++ draftivist_ui/config/webpack.config.base.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -18,21 +19,21 @@ module.exports = {
           'css-loader',
         ],
       },
-      // {
-      //   test: /\.(svg|png|jpe?g)$/,
-      //   use: [
-      //     {loader: 'file-loader',
-      //     options: {
-      //       name: '[name].[ext]',
-      //       outputPath: path.resolve(__dirname, '../public'),
-      //       publicPath: '/'
-      //     }}
-      //   ]
-      // },
-      // {
-      //   test: /\.html$/,
-      //   use: ['html-loader']
-      // }
+      {
+        test: /\.(svg|png|jpe?g)$/,
+        use: [
+          {loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: path.resolve(__dirname, '../dist'),
+            publicPath: '/'
+          }}
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
     ],
   },
   plugins: [
@@ -42,15 +43,23 @@ module.exports = {
       filename: 'splash.html',
       template: path.resolve(__dirname, '../public/splash.html'),
       inject: false,
-      // publicPath: '/'
+      publicPath: '/'
     }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/assets/images',
+          to: '../public/assets/images'
+        }
+      ]
+    })
   ],
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist',),
-    // publicPath: '/'
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
   },
 };
