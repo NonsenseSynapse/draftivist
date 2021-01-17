0dc79fe5ccebebe2a14486062427502585ff963c
Keith Newman <keithnewman10@gmail.com>
2021-01-17 13:02:21 -0800

    added prod API host

--- draftivist_ui/config/webpack.config.production.js
+++ draftivist_ui/config/webpack.config.production.js
@@ -1,12 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.base');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
         new webpack.DefinePlugin({
-          '__API_HOSTNAME__': "**FILL THIS IN**"
+          '__API_HOSTNAME__': JSON.stringify("https://draftivist.com/api")
         })
       ]
 })
