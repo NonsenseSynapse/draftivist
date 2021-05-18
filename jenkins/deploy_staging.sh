f56acc587f0e63c25427298c4af9432650ec971d
Keith Newman <keithnewman10@gmail.com>
2021-05-17 20:43:45 -0700

    run container with docker-compose

--- jenkins/deploy_staging.sh
+++ jenkins/deploy_staging.sh
@@ -9,14 +9,11 @@ fi
 VERSION_TAG=$1
 
 ssh -t root@staging.draftivist.com -o StrictHostKeyChecking=no \
-  echo "Hello I am here" && \
-  echo ${VERSION_TAG}
-
-# echo "Pulling latest image from DockerHub..." && \
-# docker pull nonsensesynapse/draftivist:${VERSION_TAG} \
-# echo "Stopping existing container..." && \
-# echo "TODO: STOP EXISTING CONTAINER"\
-# docker-compose -f /root/draftivist/docker-compose.staging.yaml
+ echo "Pulling latest image from DockerHub..." && \
+ docker pull nonsensesynapse/draftivist:${VERSION_TAG} \
+ echo "Stopping existing container..." && \
+ echo "TODO: STOP EXISTING CONTAINER"\
+ docker-compose -f /root/draftivist/docker-compose.staging.yaml up -d
 
 # docker run -d --env-file /root/draftivist/.env --expose 8000:8000
 
