1b78c77672478ae7128d116f2504c74eb8de0dce
Keith Newman <keithnewman10@gmail.com>
2021-05-17 20:44:37 -0700

    missing &&

--- jenkins/deploy_staging.sh
+++ jenkins/deploy_staging.sh
@@ -10,9 +10,9 @@ VERSION_TAG=$1
 
 ssh -t root@staging.draftivist.com -o StrictHostKeyChecking=no \
  echo "Pulling latest image from DockerHub..." && \
- docker pull nonsensesynapse/draftivist:${VERSION_TAG} \
+ docker pull nonsensesynapse/draftivist:${VERSION_TAG} && \
  echo "Stopping existing container..." && \
- echo "TODO: STOP EXISTING CONTAINER"\
+ echo "TODO: STOP EXISTING CONTAINER" && \
  docker-compose -f /root/draftivist/docker-compose.staging.yaml up -d
 
 # docker run -d --env-file /root/draftivist/.env --expose 8000:8000
