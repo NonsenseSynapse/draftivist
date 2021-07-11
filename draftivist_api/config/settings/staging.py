307802203f3df7b8d24a505766b08df12d398597
Keith Newman <keithnewman10@gmail.com>
2021-07-11 16:59:05 -0700

    rename remaining instances of cyoa to config; dedicated settings file for staging

--- draftivist_api/config/settings/staging.py
+++ draftivist_api/config/settings/staging.py
@@ -1,14 +1,14 @@
 import os
 from .base import PROJECT_ROOT
 
-SECRET_KEY = 'b)7$nz1dt8f)3!la#w8cvkr)vb0v04p1r=+s8@a%&7+wswr0i)'
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+    'http://localhost:8000',
 ]
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
