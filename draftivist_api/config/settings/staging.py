a26c4802e1058c939bf595626182f47179900ae5
Keith Newman <keithnewman10@gmail.com>
2021-07-13 21:08:28 -0700

    test to get static files serving properly

--- draftivist_api/config/settings/staging.py
+++ draftivist_api/config/settings/staging.py
@@ -2,13 +2,14 @@ import os
 from .base import PROJECT_ROOT
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['staging.draftivist.com']
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
+    'http://staging.draftivist.com'
 ]
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
